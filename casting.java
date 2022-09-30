ea15ad4ae56c5ee2d798a431ecb80a6a99863d7c
12154 <joshua.gelfand@vtsd.com>
2022-09-30 10:12:01 -0400

    100 point version jg

---
+++ casting.java
@@ -0,0 +1,15 @@
+public class casting {
+    public static void main(String args[]) {
+        int dogs = (int) 4.5;
+        double exactDogs = 7.2;
+        char nicksfav = 'S';
+        System.out.println(dogs);
+        System.out.println(exactDogs);
+        System.out.println((int)nicksfav);
+        nicksfav++;
+        System.out.println(nicksfav);
+
+        boolean sadness = false;
+        System.out.println(sadness);
+    }
+}
